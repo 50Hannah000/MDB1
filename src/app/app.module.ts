8678061298f1d5972a57a21b46ffd1174366d762
Hannah Mauritz <hannahmauritz@gmail.com>
2018-03-29 01:08:28 +0200

    added cameraprovider and camarapage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,13 +9,18 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ListItemPage } from '../pages/list-item/list-item';
+import { PicturePage } from '../pages/picture/picture';
 import { BaseHeaderComponent } from '../components/base-header/base-header'
+import { CameraProvider } from '../providers/camera/camera';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
     ListPage,
     ListItemPage,
+    PicturePage,
     BaseHeaderComponent
   ],
   imports: [
@@ -27,12 +32,15 @@ import { BaseHeaderComponent } from '../components/base-header/base-header'
     MyApp,
     HomePage,
     ListPage,
-    ListItemPage
+    ListItemPage,
+    PicturePage
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    CameraProvider,
+    Camera
   ]
 })
 export class AppModule {}
