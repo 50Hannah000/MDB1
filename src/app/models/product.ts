4e31620af2e86ec158ee442e64b5dfce277944d6
Hannah Mauritz <hannahmauritz@gmail.com>
2018-06-14 14:48:38 +0200

    fix listitem

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,6 +1,14 @@
 export class Product {
+<<<<<<< HEAD
     _id: string;
     name: string;
     imagePath: string;
     price: number;
   }
+=======
+  _id: string;
+  name: string;
+  imagePath: string;
+  price: number;
+}
+>>>>>>> fix listitem
