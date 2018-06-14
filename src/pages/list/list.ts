fcd8e7b7da22897031c1a9d149c356a2b77a0ec1
Martijn Coomans <mcoomans@avans.nl>
2018-06-14 14:47:02 +0200

    added product model

--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ListItemPage } from '../list-item/list-item';
+import { Product } from '../../app/models/product';
+import { ProductsProvider } from '../../providers/products/products';
 
 @IonicPage()
 @Component({
@@ -10,31 +12,36 @@ import { ListItemPage } from '../list-item/list-item';
 export class ListPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  products: Product[];
+  private page = 0;
+  private limit = 20;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
+  constructor(public navCtrl: NavController, public navParams: NavParams, public productProvider: ProductsProvider) {
+    
+  }
 
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
-    this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
-    }
+  ionViewDidEnter() {
+    this.fetchNewPage();
   }
 
   itemTapped(event, item) {
-    console.log('listts', item)
-    // That's right, we're pushing to ourselves!
     this.navCtrl.push(ListItemPage, {
       item: item
     });
+  }
+
+  protected doInfinite(infiniteScroll) {
+    setTimeout(() => {
+      this.fetchNewPage();
+      infiniteScroll.complete();
+    }, 500);
+  }
+
+  private fetchNewPage() {
+    this.page++;
+
+    console.log(this.productProvider.getAllProducts(this.limit, this.page));
+
+    
   }
 }
--- src/providers/products/products.ts
+++ src/providers/products/products.ts
@@ -3,18 +3,35 @@ import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { AuthenticationProvider } from '../authentication/authentication';
 import { Storage } from '@ionic/storage';
+import { Product } from '../../app/models/product';
 
 @Injectable()
 export class ProductsProvider {
-  products: any;
   currentToken: string;
 
   constructor(public http: Http, private auth: AuthenticationProvider, private storage: Storage) {
-    
+
    }
 
-  getAllProducts()
+  getAllProducts(limit?: Number, page?: Number)
   {
+      var params = '';
+      
+      if((limit && limit > 0) || (page && page > 0)) {
+        params += '?';
+      }
+
+      if(limit && limit > 0) {
+        params += 'limit=' + limit;
+      }
+
+      if(page && page > 0) {
+        if(limit && limit > 0){
+          params += '&';
+        }
+        params += 'page=' + page;
+      }
+
       return this.storage.get('currentToken').then(token => {
         const headers = new Headers();
         headers.append('Content-Type', 'application/json');
@@ -26,11 +43,20 @@ export class ProductsProvider {
         };
 
         return new Promise(resolve => {
-          this.http.get(this.auth.getBaseUrl() + '/products', httpOptions).map(res => res.json())
+          this.http.get(this.auth.getBaseUrl() + '/products' + params, httpOptions).map(res => res.json())
               .subscribe(data => {
-                  resolve(data.products);
+                let products: Product[] = [];
+                data.products.forEach(product => {
+                  products.push({
+                    _id: product._id,
+                    name: product.name,
+                    imagePath: product.imagePath,
+                    price: product.price
+                  });
+                });
+                resolve(products);
                 }, err => {
-                  console.log(err);
+                  
                 }
               );
       });
