c8e33d41ddd8f88b2e9437a54419a74d1138694d
Hannah Mauritz <hannahmauritz@gmail.com>
2018-06-14 23:07:25 +0200

    maps tryout

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,6 +6,16 @@ import { AuthenticationProvider } from '../../providers/authentication/authentic
 import { ProductsProvider } from '../../providers/products/products';
 import { User } from '../../app/models/user';
 import { Storage } from '@ionic/storage';
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker
+} from '@ionic-native/google-maps';
+
 
 @IonicPage()
 @Component({
@@ -15,11 +25,13 @@ import { Storage } from '@ionic/storage';
 export class HomePage {
   user: User;
   products: any;
+  map: GoogleMap;
 
   constructor(private storage: Storage, public navCtrl: NavController, public auth: AuthenticationProvider, public productProvider: ProductsProvider) { }
 
   ionViewDidLoad() {
-
+    this.loadMap();
+    
     this.storage.get('currentUser').then((user) => {
       this.user = user;
     });
@@ -35,4 +47,34 @@ export class HomePage {
   goToCreate() {
     this.navCtrl.push("CreateItemPage");
   }
+
+  loadMap() {
+
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: 43.0741904,
+           lng: -89.3809802
+         },
+         zoom: 18,
+         tilt: 30
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: 'Ionic',
+      icon: 'blue',
+      animation: 'DROP',
+      position: {
+        lat: 43.0741904,
+        lng: -89.3809802
+      }
+    });
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      alert('clicked');
+    });
+  }
 }
+
